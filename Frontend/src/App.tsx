914bc38db14fea7690875481250156b4ae1377f2
pshycodr <115141578+pshycodr@users.noreply.github.com>
2025-01-22 15:23:32 +0530

    redirect / route

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -12,6 +12,7 @@ function App() {
         <>
             <BrowserRouter>
                 <Routes>
+                    <Route path="/" element={<Signup/>} />
                     <Route path="/signin" element={<Signin/>} />
                     <Route path="/signup" element={<Signup/>} />
                     <Route path="/blogs" element={<Blogs/>} />
